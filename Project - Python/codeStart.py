b6a2d2c411b5eb4b84d702d01f76ca3f850ffcb6
higor21 <felype.cet.15@outlook.com>
2018-10-22 22:30:54 -0300

    atualizando projeto de Redes -> depois eu acrescento mais comentário :)

--- Project - Python/codeStart.py
+++ Project - Python/codeStart.py
@@ -1,50 +1,97 @@
+#coding: utf-8
+'''################################## DÚVIDAS
+    * Os dados de cada cliente deve permanecer na lista caso ele dislogue?
+    * Explicação relacionada aos 'bytes' da mensagem 
+    * Todos os 'nicknames' devem ser distintos?
+'''##################################
 from socket import *
+from threading import Thread
 
-serverName = '10.0.0.14' #Meu IP atual, deve ser verificado sempre
+serverName = '192.168.0.9' #Meu IP atual, deve ser verificado sempre
 serverPort = 12000
 
-#cria o socket com IPv4 (AF_INET) usando TCP (SOCK_STREAM)
 serverSocket = socket(AF_INET,SOCK_STREAM)
-
-#permite que seja possivel reusar o endereco e porta do servidor caso seja encerrado incorretamente
 serverSocket.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
-
-#Vincula o socket com a porta
 serverSocket.bind((serverName,serverPort))
 
 serverSocket.listen(100)
 print('The server has started... \n')
 
-
+# lista de usuário 
 listClients = {}
 
-"""Função para definir o nick name do cliente"""
-def nick_name(cliCon)
-    # requisita o nick_name do novo cliente
-    cliCon.send(raw_input("Por favor, informe seu nickname: "))
-    # nick_name digitado pelo cliente
-    nick = cliCon.recv(1024).decode('utf-8')
-    
 
-def chat(cliCon,cliAddr):
-    ''' verifica se o cliente já existe: 
-        * caso exista: continua o programa
-        * caso contrário: pergunta o nick_name para o usuário
-    '''
-    if(cliAddr not in listClients)
-        listClients[cliAddr] = nick_name(cliCon)
+class Message:
+    # construtor
+    def __init__(self, ip_o = '', ip_d = '', nickname = '', command = '', msg = ''):
+        self.ip_o, self.ip_d, self.msg  = ip_o, ip_d, msg
+        self.nickname, self.command,= nickname, command
+        self.size = sys.getsizeof(self) # tamanho da mensagem (incluindo a cabeçalho)
 
+    def __str__(self):
+        # definir melhor depois como será impressa a mensagem ...
+        return 'orig: ' + self.ip_o + ', dest: ' + self.ip_d + ' -> message:\n\t' + self.msg
 
-    message = cliCon.recv(1024).decode('utf-8')
-    print('Client escreveu: \n', message)
+''' FALTA FAZER O USUÁRIO '''
+class User:
+    # construtor
+    def __init__(self, cliCon, nickname, disp = False, isLogged = True):
+        pass
+
+class Chat(Thread):
+    # construtor
+    def __init__(self, cliCon, cliAddr, listM = ()):
+        Thread.__init__(self)
+        self.cliCon = cliCon
+        self.cliAddr = cliAddr
+        self.listMessages = listM # lista de mensagens do cliente (caso ele acabe de entrar e já tenha mensagens para o mesmo)
+        if(cliAddr not in listClients):
+            cliCon.sendto("Por favor, informe seu nickname: ", cliAddr)
+            nick = cliCon.recv(1024).decode('utf-8')
+            # 'False' indica que o usuário não está no privado com ninguém 
+            # 'True' indica que o usuário está logado
+            listClients[cliAddr] = User(cliCon, nick)
+            print listClients[cliAddr]
+        else: 
+            listClients[cliAddr].isLogged = True
+            listClients[cliAddr].cliCon = cliCon
+        self.sendData(listClients[cliAddr].nickname + ' entrou...')
+        Thread(target=self.receiveData).start() # espera por mensagem do usuário 
+
+    def sendData(self, msg = ''): # 'msg' representa a mensagem 'nick-name entrou...'
+        if msg != '':
+            pass # AINDA FALTA FAZER 
+        
+    def receiveData(self):
+        while(True):
+            message = self.cliCon.recv(1024).decode('utf-8')
+            cmd = message.command
+            print('Client escreveu: \n', message)
+
+            if cmd == 'lista()':
+                self.cliCon.sendto(listClients,self.cliAddr)
+            elif cmd.startswith('privado(') and cmd.endswith(')'): # verifica se o comando é privado(*)
+                # verifica se o 'nickname' existe 
+                if message.nickname[len('privado('):len(message.nickname)-1] == listClients[message.ip_d] :
+                    '''
+                        CRIAR UM CLAT PRIVADO (OU FAZER ESSE SITE TER DOIS MODOS, PRIVADO E PÚBLICO) E CHAMAR UMA THREAD AQUI
+                    '''
+            elif cmd.startswith('nome(') and cmd.endswith(')'): # verifica se o comando é nome(*)
+                new_nickname = message.nickname[len('nome('):len(message.nickname)-1]
+                self.sendData(listClients[message.ip_o].nickname + ' agora é ' + new_nickname)
+                listClients[message.ip_o].nickname = message.nickname[len('nome('):len(message.nickname)-1]
+                
+            elif cmd == 'sair()': 
+                listClients[message.ip_o].isLogged = False
+                self.sendData(listClients[message.ip_o].nickname + 'saiu!')
+                self.cliCon.close()
+                break
+            else:
+                self.sendData('comando inválido!\n')
 
 while 1:
-    #Espera por novas conexões
-    # cliCon é um novo socket criado para o cliente que acabou de se conectar
-    # cliAddr é o endereço do cliente que acabou de se conectar
     cliCon, cliAddr = serverSocket.accept()
-
-    # inicia um novo chat para o cliente
-    threading.Thread(target=chat, args=(cliCon, cliAddr)).start()
+    print "dafdf"
+    Chat(cliCon, cliAddr).start()
 
     
